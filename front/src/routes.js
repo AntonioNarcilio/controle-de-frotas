7ffbc0dd55ea57ad9ad5c351e8fceef56c522e1a
Antonio Narclio <antonionarcilio@gmail.com>
2020-11-23 21:45:09 -0300

    Atualizando o README.md

--- front/src/routes.js
+++ front/src/routes.js
@@ -1,11 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-// const path = require('path')
-
-
 router.get('/', (req, res) => {
-	// res.sendFile(path.join(__dirname, 'views', 'home', 'index.html'))
 	return res.render('home/index.html',
 	{title: 'Home'})
 })
